4d99d156588b85898dd1cc00bf62f80ee0f42df4
Sophie Cao <sophiecaoxin@gmail.com>
2024-09-23 15:45:12 +1200

    Updates

---
+++ precache-manifest.501fbef953a44c442a1ca4f9e6089c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fcc1b5741a2055feaa45f608ae62c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "080d0e12208d5d012f7e",
+    "url": "/static/css/main.3d0a987d.chunk.css"
+  },
+  {
+    "revision": "a13315a26a41a8d646d8",
+    "url": "/static/js/2.61de123b.chunk.js"
+  },
+  {
+    "revision": "0995f9ec7b10de470a1751b7e63002f5",
+    "url": "/static/js/2.61de123b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf01faf74506153b26f",
+    "url": "/static/js/3.24426002.chunk.js"
+  },
+  {
+    "revision": "080d0e12208d5d012f7e",
+    "url": "/static/js/main.e60294f0.chunk.js"
+  },
+  {
+    "revision": "b456d983b041fc1899b9",
+    "url": "/static/js/runtime-main.6ab39ba8.js"
+  }
+]);
