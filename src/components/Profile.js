6bcb22595e1f4069daed7163e52a885093072178
Sophie Cao <sophiecaoxin@gmail.com>
2024-09-23 17:49:42 +1200

    added google tag manager code snippets

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,9 +8,24 @@ import { Helmet } from 'react-helmet';
 const Profile = () => {
   return (
     <div>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HGWTS48"
+            height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe>
+      </noscript>
+
       <Helmet>
-          <title>Sophie Codes</title>
-        </Helmet>
+        <title>Sophie Codes</title>
+        {/* Google Tag Manager */}
+        <script>
+         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
+         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
+         })(window,document,'script','dataLayer','GTM-5HGWTS48');`}
+        </script>
+     </Helmet>
+
+
       {/* START NAV */}
       <nav id="navbar" className="nav">
         <ul className="nav-list">
