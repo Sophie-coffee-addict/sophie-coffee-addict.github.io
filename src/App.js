0e49a0d518142c9c49d9a3342d37cf34d1556cf0
Sophie Cao <sophiecaoxin@gmail.com>
2024-09-24 16:56:28 +1200

    fixed routing and hash link handling for GitHub Pages by implementing HashRouter

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <title>Sophie Codes</title>
       </Helmet>
       <div className="App">
+        {/* 使用 HashRouter 来确保在 GitHub Pages 上正确处理路由 */}
         <Routes>
           <Route path="/" element={<Profile />} />
           <Route path="/steve-jobs" element={<SteveJobsPage />} />
